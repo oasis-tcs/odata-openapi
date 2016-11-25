4cbffc2363f9227dcc20007063bf5fa1fb385ec3
Ralf Handl <ralf.handl@sap.com>
2016-11-25 23:53:51 +0100

    Petstore Swagger UI instead of local installation

--- tools/transform.cmd
+++ tools/transform.cmd
@@ -26,8 +26,7 @@ exit /b
 
   echo %~n1
 
-  @rem -PARAM extensions YES -PARAM odata-schema http://localhost/schemas/edm.json
-  java.exe org.apache.xalan.xslt.Process -XSL V4-CSDL-to-openapi.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM diagram YES -IN ..\examples\%1 -OUT %~n1.jsontmp
+  java.exe org.apache.xalan.xslt.Process -XSL V4-CSDL-to-openapi.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM swagger-ui http://petstore.swagger.io -PARAM diagram YES -IN ..\examples\%1 -OUT %~n1.jsontmp
 
   c:\git\yajl\build\yajl-2.1.1\bin\json_reformat.exe < %~n1.jsontmp > ..\examples\%~n1.openapi.json
   if not errorlevel 1 (
