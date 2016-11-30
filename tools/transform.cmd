9ba0d83896c811ccdd67d4d7d985bea495883841
Ralf Handl <ralf.handl@sap.com>
2016-11-30 18:37:30 +0100

    Added V2 example

--- tools/transform.cmd
+++ tools/transform.cmd
@@ -6,13 +6,13 @@ setlocal
 set CLASSPATH=%CLASSPATH%;C:\eclipse-Neon\plugins\org.apache.xml.serializer_2.7.1.v201005080400.jar;C:\eclipse-Neon\plugins\org.apache.xalan_2.7.1.v201005080400.jar
 set done=false
 
-for /F "eol=# tokens=1,2,3,4" %%F in (%~n0.txt) do (
-	if /I [%~n1] == [%%~nF] (
+for /F "eol=# tokens=1,2,3,4,5" %%F in (%~n0.txt) do (
+	if /I [%~n1]==[%%~nF] (
 	    set done=true
-		call :process %%F %%G %%H %%I
+		call :process %%F %%G %%H %%I %%J
 	) else if [%1]==[] (
 	    set done=true
-		call :process %%F %%G %%H %%I
+		call :process %%F %%G %%H %%I %%J
 	)
 )
 
@@ -23,14 +23,24 @@ exit /b
 
 
 :process
-
   echo %~n1
+  
+  if [%5]==[V2] (
+    java.exe org.apache.xalan.xslt.Process -XSL V2-to-V4-CSDL.xsl -IN ..\examples\%1 -OUT %~n1.V4.xml
+    set VERSION=2.0
+    set INPUT=%~n1.V4.xml
+  ) else (
+    set VERSION=4.0
+    set INPUT=..\examples\%1
+  )
 
-  java.exe org.apache.xalan.xslt.Process -XSL V4-CSDL-to-openapi.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM swagger-ui http://petstore.swagger.io -PARAM diagram YES -IN ..\examples\%1 -OUT %~n1.jsontmp
+  java.exe org.apache.xalan.xslt.Process -XSL V4-CSDL-to-openapi.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM odata-version %VERSION% -PARAM swagger-ui http://petstore.swagger.io -PARAM diagram YES -IN %INPUT% -OUT %~n1.jsontmp
 
   c:\git\yajl\build\yajl-2.1.1\bin\json_reformat.exe < %~n1.jsontmp > ..\examples\%~n1.openapi.json
   if not errorlevel 1 (
     del %~n1.jsontmp
+    if [%5]==[V2] del %~n1.V4.xml
     git.exe --no-pager diff ..\examples\%~n1.openapi.json
   )
+
 exit /b
