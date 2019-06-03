adf56a03af06e2a3f032b941d4b6776cec3c811e
Ralf Handl <ralf.handl@sap.com>
2019-06-03 11:22:29 +0200

    Cleaner exports Adapted to changes in odata-csdl

--- test/csdl2openapi.test.js
+++ test/csdl2openapi.test.js
@@ -2,16 +2,16 @@ const assert = require('assert');
 const fs = require('fs');
 
 //TODO: generate JSON input once
-const xml2json = require('odata-csdl-json');
-const csdl2openapi = require('../lib/csdl2openapi');
+const csdl = require('odata-csdl');
+const lib = require('../lib/csdl2openapi');
 
-const example1 = xml2json(fs.readFileSync('examples/csdl-16.1.xml'));
+const example1 = csdl.xml2json(fs.readFileSync('examples/csdl-16.1.xml'));
 const result1 = require('../examples/csdl-16.1.openapi3.json');
 
 describe('Examples', function () {
 
     it('csdl-16.1', function () {
-        assert.deepStrictEqual(csdl2openapi(example1), result1);
+        assert.deepStrictEqual(lib.csdl2openapi(example1), result1);
     })
 
 })
