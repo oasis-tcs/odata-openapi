f0cd7099dc3a918c4426748039a97655de6f9159
Ralf Handl <ralf.handl@sap.com>
2019-06-06 16:20:47 +0200

    Started second test case: TripPin

--- test/csdl2openapi.test.js
+++ test/csdl2openapi.test.js
@@ -8,6 +8,9 @@ const lib = require('../lib/csdl2openapi');
 const example1 = csdl.xml2json(fs.readFileSync('examples/csdl-16.1.xml'));
 const result1 = require('../examples/csdl-16.1.openapi3.json');
 
+const example2 = csdl.xml2json(fs.readFileSync('examples/TripPin.xml'));
+const result2 = require('../examples/TripPin.openapi3.json');
+
 describe('Examples', function () {
 
     it('csdl-16.1', function () {
@@ -15,4 +18,12 @@ describe('Examples', function () {
         assert.deepStrictEqual(openapi, result1, 'CSDL specification example');
     })
 
+    it('TripPin', function () {
+        let openapi = lib.csdl2openapi(example2, {
+            host: 'services.odata.org',
+            basePath: '/V4/(S(cnbm44wtbc1v5bgrlek5lpcc))/TripPinServiceRW',
+            diagram: true
+        });
+        assert.deepStrictEqual(openapi, result2, 'CSDL specification example');
+    })
 })
