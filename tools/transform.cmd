d161922d6f0eafe37b641c2ff2bf10970f8c512b
Ralf Handl <ralf.handl@sap.com>
2019-07-10 15:42:04 +0200

    Validate against official JSON Schema for OpenAPI 3

--- tools/transform.cmd
+++ tools/transform.cmd
@@ -13,7 +13,7 @@ set CLASSPATH=xalan/xalan.jar;xalan/serializer.jar
 @rem  - Node.js is installed - download from https://nodejs.org/
 @rem  - ajv-cli is installed - npm install -g ajv-cli
 @rem  - https://github.com/OAI/OpenAPI-Specification is cloned next to this project
-set SCHEMA_THREE=openapi-3.0.0.schema.json
+set SCHEMA_THREE=..\..\OpenAPI-Specification\schemas\v3.0\schema.json
 set SCHEMA_TWO=..\..\OpenAPI-Specification\schemas\v2.0\schema.json
 
 set done=false
@@ -67,7 +67,7 @@ exit /b
     del ..\examples\%~n1.tmp3.json
     git.exe --no-pager diff ..\examples\%~n1.openapi3.json
 
-    if exist %SCHEMA_THREE% call ajv validate --unknown-formats=uriref -s %SCHEMA_THREE% -d ..\examples\%~n1.openapi3.json > nul
+    if exist %SCHEMA_THREE% call ajv validate -s %SCHEMA_THREE% -d ..\examples\%~n1.openapi3.json > nul
   )
 
   java.exe org.apache.xalan.xslt.Process -L -XSL V4-CSDL-to-openapi.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM odata-version %VERSION% -PARAM diagram YES -PARAM references "%6" -PARAM openapi-root "https://raw.githubusercontent.com/oasis-tcs/odata-openapi/master/examples/" -IN %INPUT% -OUT ..\examples\%~n1.tmp.json
