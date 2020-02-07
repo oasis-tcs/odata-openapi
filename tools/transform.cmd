4ade99135a11baab6d3c47efab44513ac59609d7
Ralf Handl <ralf.handl@sap.com>
2020-02-07 08:49:33 +0100

    repaired transform.cmd

--- tools/transform.cmd
+++ tools/transform.cmd
@@ -1,5 +1,7 @@
 @echo off 
 setlocal
+set here=%~dp0
+
 
 @rem  This script uses the Apache Xalan 2.7.2 XSLT processor
 @rem  For a description of Xalan command-line parameters see http://xalan.apache.org/xalan-j/commandline.html
@@ -8,13 +10,12 @@ setlocal
 @rem  - Java SE is installed and in the PATH - download from http://www.oracle.com/technetwork/java/javase/downloads/index.html 
 @rem  - git is installed and in the PATH - download from https://git-for-windows.github.io/
 @rem    Xalan is installed and CLASSPATH contains xalan.jar and serializer.jar - download from http://xalan.apache.org/xalan-j/downloads.html
-set CLASSPATH=xalan/xalan.jar;xalan/serializer.jar
+set CLASSPATH=%here%xalan/xalan.jar;%here%xalan/serializer.jar
 @rem  - YAJL's json_reformat from https://github.com/lloyd/yajl has been compiled and is in the PATH
 @rem  - Node.js is installed - download from https://nodejs.org/
 @rem  - ajv-cli is installed - npm install -g ajv-cli
 @rem  - https://github.com/OAI/OpenAPI-Specification is cloned next to this project
-set SCHEMA_THREE=..\..\OpenAPI-Specification\schemas\v3.0\schema.json
-set SCHEMA_TWO=..\..\OpenAPI-Specification\schemas\v2.0\schema.json
+set SCHEMA_THREE=%here..\..\OpenAPI-Specification\schemas\v3.0\schema.json
 
 if exist "%1" (
   if /I [%2]==[V2] (
@@ -38,29 +39,33 @@ exit /b
   echo %~n1
   
   if [%5]==[V2] (
-    java.exe org.apache.xalan.xslt.Process -L -XSL V2-to-V4-CSDL.xsl -IN ..\examples\%1 -OUT ..\examples\%~n1.V4.xml
+    java.exe org.apache.xalan.xslt.Process -L -XSL %here%V2-to-V4-CSDL.xsl -IN %1 -OUT %~dpn1.V4.xml
     set VERSION=2.0
-    set INPUT=..\examples\%~n1.V4.xml
+    set INPUT=%~dpn1.V4.xml
   ) else if [%5]==[V3] (
-    java.exe org.apache.xalan.xslt.Process -L -XSL V2-to-V4-CSDL.xsl -IN ..\examples\%1 -OUT ..\examples\%~n1.V4.xml
+    java.exe org.apache.xalan.xslt.Process -L -XSL %here%V2-to-V4-CSDL.xsl -IN %1 -OUT %~dpn1.V4.xml
     set VERSION=3.0
-    set INPUT=..\examples\%~n1.V4.xml
+    set INPUT=%~dpn1.V4.xml
   ) else (
     set VERSION=4.0
-    set INPUT=..\examples\%1
+    set INPUT=%1
   )
 
-  java.exe org.apache.xalan.xslt.Process -L -XSL V4-CSDL-to-OpenAPI.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM odata-version %VERSION% -PARAM diagram YES -PARAM references "%6" -PARAM openapi-root "https://raw.githubusercontent.com/oasis-tcs/odata-openapi/master/examples/" -PARAM openapi-version 3.0.0 -IN %INPUT% -OUT ..\examples\%~n1.tmp3.json
+  java.exe org.apache.xalan.xslt.Process -L -XSL %here%V4-CSDL-to-OpenAPI.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM odata-version %VERSION% -PARAM diagram YES -PARAM references "%6" -PARAM openapi-root "https://raw.githubusercontent.com/oasis-tcs/odata-openapi/master/examples/" -PARAM openapi-version 3.0.0 -IN %INPUT% -OUT %~dpn1.tmp3.json
 
-  json_reformat.exe < ..\examples\%~n1.tmp3.json > ..\examples\%~n1.openapi3.json
+  json_reformat.exe < %~dpn1.tmp3.json > %~dpn1.openapi3.json
   if not errorlevel 1 (
-    del ..\examples\%~n1.tmp3.json
-    if [%5]==[V2] del ..\examples\%~n1.V4.xml
-    if [%5]==[V3] del ..\examples\%~n1.V4.xml
+    del %~dpn1.tmp3.json
+    if [%5]==[V2] del %~dpn1.V4.xml
+    if [%5]==[V3] del %~dpn1.V4.xml
 
-    git.exe --no-pager diff ..\examples\%~n1.openapi3.json
+    pushd .
+    %~d1
+    cd %~p1
+    git.exe --no-pager diff %~n1.openapi3.json 2>nul
+    popd
 
-    if exist %SCHEMA_THREE% call ajv validate -s %SCHEMA_THREE% -d ..\examples\%~n1.openapi3.json > nul
+    if exist %SCHEMA_THREE% call ajv validate -s %SCHEMA_THREE% -d %~dpn1.openapi3.json > nul
   )
 
 exit /b
