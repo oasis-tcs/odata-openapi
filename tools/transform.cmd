197825c2830d50c38a4265e3adde2450a2b8d3d1
Ralf Handl <ralf.handl@sap.com>
2020-02-10 09:51:52 +0100

    transform.cmd: auto-detect OData version

--- tools/transform.cmd
+++ tools/transform.cmd
@@ -2,7 +2,6 @@
 setlocal
 set here=%~dp0
 
-
 @rem  This script uses the Apache Xalan 2.7.2 XSLT processor
 @rem  For a description of Xalan command-line parameters see http://xalan.apache.org/xalan-j/commandline.html
 @rem
@@ -16,18 +15,15 @@ set CLASSPATH=%here%xalan/xalan.jar;%here%xalan/serializer.jar
 @rem  - ajv-cli is installed - npm install -g ajv-cli
 @rem  - https://github.com/OAI/OpenAPI-Specification is cloned next to this project
 set SCHEMA_THREE=%here..\..\OpenAPI-Specification\schemas\v3.0\schema.json
+set SCHEMA_TWO=%here..\..\OpenAPI-Specification\schemas\v2.0\schema.json
 
 if exist "%1" (
-  if /I [%2]==[V2] (
-    call :process %1 http localhost /service-root V2
-  ) else (
-    call :process %1 http localhost /service-root V4
-  )
+  call :process %1 http localhost /service-root %2
 ) else (
-  echo Usage: transform source [V2]
+  echo Usage: transform source [/swagger]
   echo.
   echo   source       Specifies the file to be transformed.
-  echo   V2           Indicates that source is OData V2 or OData V3
+  echo   /swagger     Output Swagger 2.0 in addition to OpenAPI 3.0.0
 
 )
 
@@ -38,26 +34,42 @@ exit /b
 :process
   echo %~n1
   
-  if [%5]==[V2] (
+  for /f %%V in ('java.exe org.apache.xalan.xslt.Process -XSL %here%OData-Version.xsl -IN %1') do set ODATA_VERSION=%%V
+
+  if [%ODATA_VERSION%]==[2.0] (
     java.exe org.apache.xalan.xslt.Process -L -XSL %here%V2-to-V4-CSDL.xsl -IN %1 -OUT %~dpn1.V4.xml
-    set VERSION=2.0
     set INPUT=%~dpn1.V4.xml
-  ) else if [%5]==[V3] (
+  ) else if [%ODATA_VERSION%]==[3.0] (
     java.exe org.apache.xalan.xslt.Process -L -XSL %here%V2-to-V4-CSDL.xsl -IN %1 -OUT %~dpn1.V4.xml
-    set VERSION=3.0
     set INPUT=%~dpn1.V4.xml
   ) else (
-    set VERSION=4.0
     set INPUT=%1
   )
+  
+  if [%5]==[/swagger] (
+    java.exe org.apache.xalan.xslt.Process -L -XSL %here%V4-CSDL-to-OpenAPI.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM odata-version %ODATA_VERSION% -PARAM diagram YES -PARAM openapi-root "https://raw.githubusercontent.com/oasis-tcs/odata-openapi/master/examples/" -PARAM openapi-version 2.0 -IN %INPUT% -OUT %~dpn1.tmp2.json
 
-  java.exe org.apache.xalan.xslt.Process -L -XSL %here%V4-CSDL-to-OpenAPI.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM odata-version %VERSION% -PARAM diagram YES -PARAM references "%6" -PARAM openapi-root "https://raw.githubusercontent.com/oasis-tcs/odata-openapi/master/examples/" -PARAM openapi-version 3.0.0 -IN %INPUT% -OUT %~dpn1.tmp3.json
+    json_reformat.exe < %~dpn1.tmp2.json > %~dpn1.swagger.json
+    if not errorlevel 1 (
+      del %~dpn1.tmp2.json
+
+      pushd .
+      %~d1
+      cd %~p1
+      git.exe --no-pager diff %~n1.swagger.json 2>nul
+      popd
+
+      if exist %SCHEMA_TWO% call ajv validate -s %SCHEMA_TWO% -d %~dpn1.swagger.json > nul
+    )
+  )
+
+  java.exe org.apache.xalan.xslt.Process -L -XSL %here%V4-CSDL-to-OpenAPI.xsl -PARAM scheme %2 -PARAM host %3 -PARAM basePath %4 -PARAM odata-version %ODATA_VERSION% -PARAM diagram YES -PARAM openapi-root "https://raw.githubusercontent.com/oasis-tcs/odata-openapi/master/examples/" -PARAM openapi-version 3.0.0 -IN %INPUT% -OUT %~dpn1.tmp3.json
 
   json_reformat.exe < %~dpn1.tmp3.json > %~dpn1.openapi3.json
   if not errorlevel 1 (
     del %~dpn1.tmp3.json
-    if [%5]==[V2] del %~dpn1.V4.xml
-    if [%5]==[V3] del %~dpn1.V4.xml
+    if [%ODATA_VERSION%]==[2.0] del %~dpn1.V4.xml
+    if [%ODATA_VERSION%]==[3.0] del %~dpn1.V4.xml
 
     pushd .
     %~d1
