efb7c9e2ff59503ebd7e7cb88078062a1f09f4f0
Ralf Handl <ralf.handl@sap.com>
2021-03-23 10:16:19 +0100

    fix: pinned ajv-cli version, OpenAPI schemas, validation (#118)
    * fix: pinned ajv-cli version, OpenAPI schemas, validation

--- tools/transform.cmd
+++ tools/transform.cmd
@@ -1,21 +1,20 @@
-@echo off 
-setlocal
-set here=%~dp0
-
 @rem  This script uses the Apache Xalan 2.7.2 XSLT processor
 @rem  For a description of Xalan command-line parameters see http://xalan.apache.org/xalan-j/commandline.html
 @rem
 @rem  Prerequisites
 @rem  - Java SE is installed and in the PATH - download from http://www.oracle.com/technetwork/java/javase/downloads/index.html 
 @rem  - git is installed and in the PATH - download from https://git-for-windows.github.io/
-@rem    Xalan is installed and CLASSPATH contains xalan.jar and serializer.jar - download from http://xalan.apache.org/xalan-j/downloads.html
-set CLASSPATH=%here%xalan/xalan.jar;%here%xalan/serializer.jar
 @rem  - YAJL's json_reformat from https://github.com/lloyd/yajl has been compiled and is in the PATH
-@rem  - Node.js is installed - download from https://nodejs.org/
-@rem  - ajv-cli is installed - npm install -g ajv-cli
-@rem  - https://github.com/OAI/OpenAPI-Specification is cloned next to this project
-set SCHEMA_THREE=%here..\..\OpenAPI-Specification\schemas\v3.0\schema.json
-set SCHEMA_TWO=%here..\..\OpenAPI-Specification\schemas\v2.0\schema.json
+@rem  - Node.js is installed and in the PATH - download from https://nodejs.org/
+@rem  - this package is installed - npm install
+
+@echo off 
+setlocal
+
+set here=%~dp0
+set CLASSPATH=%here%xalan/xalan.jar;%here%xalan/serializer.jar
+set SCHEMA_THREE=%here%node_modules/@apidevtools/openapi-schemas/schemas/v3.0/schema.json
+set SCHEMA_TWO=%here%node_modules/@apidevtools/openapi-schemas/schemas/v2.0/schema.json
 
 if exist "%1" (
   call :process %1 https localhost /service-root %2
@@ -58,7 +57,7 @@ exit /b
       git.exe --no-pager diff %~n1.swagger.json 2>nul
       popd
 
-      if exist %SCHEMA_TWO% call ajv validate -s %SCHEMA_TWO% -d %~dpn1.swagger.json > nul
+      call npx ajv validate -s %SCHEMA_TWO% -d %~dpn1.swagger.json > nul
     )
   )
 
@@ -76,7 +75,7 @@ exit /b
     git.exe --no-pager diff %~n1.openapi3.json 2>nul
     popd
 
-    if exist %SCHEMA_THREE% call ajv validate -s %SCHEMA_THREE% -d %~dpn1.openapi3.json > nul
+    call npx ajv validate -s %SCHEMA_THREE% -d %~dpn1.openapi3.json > nul
   )
 
 exit /b
