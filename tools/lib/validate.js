d10c6c69c6b1237747f67e7a64528d531a520940
Ralf Handl <ralf.handl@sap.com>
2022-04-10 10:55:28 +0200

    Switch from ajv-cli to ajv

---
+++ tools/lib/validate.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+"use strict";
+
+const fs = require("fs");
+
+const Ajv = require("ajv-draft-04");
+const addFormats = require("ajv-formats");
+const { openapiV2, openapiV3 } = require("@apidevtools/openapi-schemas");
+
+const ajv = new Ajv({ strict: false });
+addFormats(ajv);
+const validateV2 = ajv.compile(openapiV2);
+const validateV3 = ajv.compile(openapiV3);
+
+const exampleFolder = "./tests/";
+
+fs.readdirSync(exampleFolder)
+  .filter((fn) => fn.endsWith(".json"))
+  .forEach((jsonFile) => {
+    console.log(jsonFile);
+
+    const openapi = JSON.parse(
+      fs.readFileSync(exampleFolder + jsonFile, "utf8")
+    );
+
+    if (openapi.openapi) {
+      if (!validateV3(openapi)) console.log(validateV3.errors);
+    } else {
+      if (!validateV2(openapi)) console.log(validateV2.errors);
+    }
+  });
