5937ebac77f1e77bdc905d1cf4519aeff2cb776d
Paul Sterl <paul.sterl@outlook.de>
2023-02-23 15:22:46 +0100

    Suggestion: Added ability to skip the $batch path in the CLI (#230)
    * added the ability to skip the generation of the $batch path
    
    * extended tests
    
    * 0.21.0
    
    * changelog update for --skipBatchPath

--- test/csdl2openapi.test.js
+++ test/csdl2openapi.test.js
@@ -49,10 +49,24 @@ const result9 = require("../examples/odata-rw-v3.openapi3.json");
 const example10 = xml2json(fs.readFileSync("examples/odata-rw-v2.xml"));
 const result10 = require("../examples/odata-rw-v2.openapi3.json");
 
+const example11 = xml2json(fs.readFileSync("examples/PingTest_V1.xml"));
+const result11 = require("../examples/PingTest_V1.openapi3.json");
+const result11NoBatch = require("../examples/PingTest_V1.no-batch.openapi3.json");
+
 describe("Examples", function () {
   it("csdl-16.1", function () {
     const openapi = csdl2openapi(example1, { diagram: true });
     check(openapi, result1);
+  });
+
+  it("SAP PingTest with $batch path (default)", function () {
+    const openapi = csdl2openapi(example11, { diagram: true });
+    check(openapi, result11);
+  });
+
+  it("SAP PingTest without $batch path", function () {
+    const openapi = csdl2openapi(example11, { skipBatchPath: true, diagram: false });
+    check(openapi, result11NoBatch);
   });
 
   it("TripPin", function () {
