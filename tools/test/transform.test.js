ea88f0e730541689af88642986ed5f93e7a98e7b
Ralf Handl <ralf.handl@sap.com>
2024-02-15 13:18:43 +0100

    Simplify test procedure

--- tools/test/transform.test.js
+++ tools/test/transform.test.js
@@ -1,13 +1,10 @@
 const assert = require("assert");
 const exec = require("child_process").exec;
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
 
 describe("CLI", function () {
   this.timeout(20000);
-
-  const annoV2 = fs.readFileSync("tests/annotations-v2.openapi3.json", "utf8");
-  const tripPin = fs.readFileSync("tests/TripPin.openapi3.json", "utf8");
 
   it("help", async () => {
     const result = await cmd(["-h"]);
@@ -42,14 +39,15 @@ describe("CLI", function () {
     assert.equal(result.stderr, "Source file not OData: OData-Version.xsl\n");
   });
 
-  it("annotations-v2", async () => {
+  it("annotations-v2 as swagger", async () => {
     const target = "tests/annotations-v2.openapi.json";
     if (fs.existsSync(target)) fs.unlinkSync(target);
 
     const result = await cmd([
-      "-d",
+      "-du",
       "--scheme https",
       "--verbose",
+      "-o 2.0",
       "tests/annotations-v2.xml",
     ]);
 
@@ -59,32 +57,40 @@ describe("CLI", function () {
       "Checking OData version used in source file: tests/annotations-v2.xml",
       "Source file is OData version: 2.0",
       "Transforming tests/annotations-v2.xml to OData V4, target file: tests/annotations-v2.tmp",
-      "Transforming tests/annotations-v2.tmp to OpenAPI 3.0.0, target file: tests/annotations-v2.openapi.json",
+      "Transforming tests/annotations-v2.tmp to OpenAPI 2.0, target file: tests/annotations-v2.openapi.json",
+      "Deleting unused schemas",
+      "Writing target file: tests/annotations-v2.openapi.json",
       "Removing intermediate file: tests/annotations-v2.tmp",
       "Done.",
       "",
     ]);
     assert.equal(fs.existsSync(target), true);
     const actual = JSON.parse(fs.readFileSync(target, "utf8"));
-    const expected = JSON.parse(annoV2);
+    const expected = JSON.parse(
+      fs.readFileSync("tests/annotations-v2.swagger.json", "utf8"),
+    );
     assert.deepStrictEqual(actual, expected, "produced OpenAPI");
     fs.unlinkSync(target);
   });
 
-  it("TripPin", async () => {
-    const target = "tests/TripPin.openapi.json";
-    if (fs.existsSync(target)) fs.unlinkSync(target);
-
-    const result = await cmd(["-dpu", "tests/TripPin.xml"]);
-
-    assert.equal(result.code, 0);
-    assert.equal(result.stderr, "");
-    assert.equal(result.stdout, "");
-    assert.equal(fs.existsSync(target), true);
-    const actual = fs.readFileSync(target, "utf8").split("\n");
-    assert.deepStrictEqual(actual, tripPin.split("\n"), "produced OpenAPI");
-    fs.unlinkSync(target);
-  });
+  const tests = fs.readdirSync("tests").filter((fn) => fn.endsWith(".xml"));
+  for (const t of tests) {
+    const base = path.basename(t, ".xml");
+    it(base, async () => {
+      const target = `tests/${base}.openapi3.json`;
+      if (fs.existsSync(target)) fs.unlinkSync(target);
+      const result = await cmd([
+        "-dp",
+        "-t",
+        `tests/${base}.openapi3.json`,
+        `tests/${base}.xml`,
+      ]);
+      assert.equal(result.code, 0);
+      assert.equal(result.stderr, "");
+      assert.equal(result.stdout, "");
+      assert.equal(fs.existsSync(target), true);
+    });
+  }
 });
 
 function cmd(args, cwd) {
